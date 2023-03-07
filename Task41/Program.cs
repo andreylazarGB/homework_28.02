553dc8696a04602650d92525e77bc735c6943a6b
andrey lazar <andreylazarmr@gmail.com>
2023-03-07 16:14:27 +0300

    task41, task43

---
+++ Task41/Program.cs
@@ -0,0 +1,62 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.Clear();
+
+// int[] array = {0, 7, 8, -2, -2,};   Массив уже заполнен!!!
+//int[] array = FillArray(8, -5, 5);   Заполнение рандомам!!!
+int length = Prompt("Введите длинну масива: ");
+int[] array = Input(length);
+PrintArray(array);
+Console.WriteLine();
+Console.WriteLine($"В массиве количество положительных чисел равно {GetCountNumber(array)}");
+
+int Prompt(string text)
+{
+    Console.Write(text);
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+/*int[] FillArray(int length, int min, int max)    Заполнение рандомам!!!
+{
+    int[] arr = new int[length];
+    Random rnd = new Random();
+    for (int i = 0; i < length; i++)
+    {
+        arr[i] = rnd.Next(min, max);
+    }
+    return arr;
+}*/
+
+int[] Input(int length)                   // Ввод пользователем
+{
+    int[] arr = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        arr[i] = Prompt($"Введите {i+1}-й элемент массива: ");
+    }
+    return arr;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write($"[");
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+    Console.Write($"{array[array.Length - 1]}]");
+}
+
+int GetCountNumber(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+            count++;
+    }
+    return count;
+}
---
+++ Task43/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+//заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.Clear();
+
+double k1 = Prompt("Введите значение k1: ");
+double b1 = Prompt("Введите значение b1: ");
+double k2 = Prompt("Введите значение k2: ");
+double b2 = Prompt("Введите значение b2: ");
+double x = GetPointIntersectionX(k1, b1, k2, b2);
+double y = GetPointIntersectionY(k1, b1, k2, b2);
+Console.WriteLine($"Точка пересечения двух прямых: ({x}, {y})");
+
+
+double Prompt(string text)
+{
+    Console.Write(text);
+    double num = Convert.ToDouble(Console.ReadLine());
+    return num;
+}
+
+double GetPointIntersectionX(double k1, double b1, double k2, double b2)
+{
+    double x = (b2 - b1) / (k1 - k2);
+    return x;
+}
+double GetPointIntersectionY(double k1, double b1, double k2, double b2)
+{
+    double x = (b2 - b1) / (k1 - k2);
+    double y = k1 * x + b1;
+    return y;
+}
